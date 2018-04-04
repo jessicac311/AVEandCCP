600cfcd6a79a60735e265fbdeb1cf4b4e04bc819
jessicac311 <36611568+jessicac311@users.noreply.github.com>
2018-04-04 20:58:44 +0000

    Added continuous sending through fake data

--- Bluetooth-SPP_serverNew.py
+++ Bluetooth-SPP_serverNew.py
@@ -12,84 +12,88 @@ import time
 from bluetooth import *
 
 connection = False
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
 
-port = server_sock.getsockname()[1]
+while(connection == False):
+    server_sock=BluetoothSocket( RFCOMM )
+    server_sock.bind(("",PORT_ANY))
+    server_sock.listen(1)
+    port = server_sock.getsockname()[1]
 
-#unique UUID to connect with AVE Android Phone
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    #unique UUID to connect with AVE Android Phone
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-file = open("CCPtoAVE_Data.txt", "r")
+    file = open("CCPtoAVE_Data.txt", "r")
 
-advertise_service( server_sock, "AVECCPDataServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ]
-						 )
-while True:
-	if(connection == False):
-		print("Waiting for connection on RFCOMM channel %d" % port)
-		client_sock, client_info = server_sock.accept()
-		connection = True
-		print("Accepted connection from ", client_info)
-	try:
-		for aline in file:
-			client_sock.send(aline);
-			print("String that was just sent: %s" % aline)
-			
-			# Receive data back from AVE:
-			data = client_sock.recv(1024)
-			lat = data;
-			long = data;
-			
-			print("Received Latitude: %s" % lat)
-			print("Received Longitude: %s" % lon)
-				
-				# data = client_sock.recv(1024)
-            # if (data == "disconnect"):
-                # print("Client wanted to disconnect")
-                # client_sock.close()
-                # connection = False
-					 
-				# # this is the string that AVE is sending to RP3
-            # elif (data == "AVE test data!!"):
-		# print ("The Android App just sent: %s" % data)
-					
-					 # for aline in file:
-						# client_sock.send(aline);
-						# print("String that was just sent: %s" % aline)
-						
-						# # Receive data back from AVE:
-						# data = client_sock.recv(1024)
-						# values = data.split()
-						# lat = values[0]
-						# long = values[1]
-						
-						# print("Received Latitude: %s" % lat)
-						# print("Received Longitude: %s" % lon)
-						
-					 
-					 # # # this string is what RP3 sends to AVE
-                # # testData = "Straight,Forward,27,36,-2"
-					 # # # sends string to AVE
-                # # client_sock.send("RECEIVED: %s" % testData)
-					# # # Printing for testing 
-                # # print("SENT: %s" % testData)
-	    
-	except IOError:
-            print("Connection disconnected!")
-            client_sock.close()
-	    connection = False
-	    pass
-	except BluetoothError:
-		print("Something wrong with bluetooth")
-	# this may have to be changed in the future
-	# (it may be causing issues with continuous connection)
-	except KeyboardInterrupt:
-		print("\nDisconnected")
-		client_sock.close()
-		server_sock.close()
-		break
+    advertise_service( server_sock, "AVECCPDataServer",
+                       service_id = uuid,
+                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                       profiles = [ SERIAL_PORT_PROFILE ]
+                                                     )
+    while True:
+            if(connection == False):
+                    print("Waiting for connection on RFCOMM channel %d" % port)
+                    client_sock, client_info = server_sock.accept()
+                    connection = True
+                    print("Accepted connection from ", client_info)
+            try:
+                    for aline in file:
+                            client_sock.send(aline);
+                            print("String that was just sent: %s" % aline)
+                            
+                            # Receive data back from AVE:
+                            data = client_sock.recv(1024)
+                            lat,lon=data.split()
+          
+                            
+                            
+                            print("Received Latitude: %s" % lat)
+                            print("Received Longitude: %s" % lon)
+                    
+                                    
+                                    # data = client_sock.recv(1024)
+                # if (data == "disconnect"):
+                    # print("Client wanted to disconnect")
+                    # client_sock.close()
+                    # connection = False
+                                             
+                                    # # this is the string that AVE is sending to RP3
+                # elif (data == "AVE test data!!"):
+                    # print ("The Android App just sent: %s" % data)
+                                            
+                                             # for aline in file:
+                                                    # client_sock.send(aline);
+                                                    # print("String that was just sent: %s" % aline)
+                                                    
+                                                    # # Receive data back from AVE:
+                                                    # data = client_sock.recv(1024)
+                                                    # values = data.split()
+                                                    # lat = values[0]
+                                                    # long = values[1]
+                                                    
+                                                    # print("Received Latitude: %s" % lat)
+                                                    # print("Received Longitude: %s" % lon)
+                                                    
+                                             
+                                             # # # this string is what RP3 sends to AVE
+                    # # testData = "Straight,Forward,27,36,-2"
+                                             # # # sends string to AVE
+                    # # client_sock.send("RECEIVED: %s" % testData)
+                                            # # # Printing for testing 
+                    # # print("SENT: %s" % testData)
+            
+            except IOError:
+                print("Connection disconnected!")
+                client_sock.close()
+                connection = False
+                pass
+            except BluetoothError:
+                    print("Something wrong with bluetooth")
+            # this may have to be changed in the future
+            # (it may be causing issues with continuous connection)
+            except KeyboardInterrupt:
+                    print("\nDisconnected")
+                    client_sock.close()
+                    server_sock.close()
+                    break
+            server_sock.close()
 
